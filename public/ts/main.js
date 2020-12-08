f04905de7347db2e0c156267e47acb2407a0294b
Richard Schrader <45096112+schraderrs@users.noreply.github.com>
2020-12-08 12:13:37 +0100

    dsadasd
    adad

--- public/ts/main.js
+++ public/ts/main.js
@@ -1,27 +1,37 @@
-const app = express(); 
+
+
 
 const inputFieldFirstName = document.getElementById('input-firstname')
 const inputFieldLastName = document.getElementById('input-lastname')
 
 function ShowUsers() {
+    
+    
     // const User = function (firstname, lastname) {
     //     this.firstname = firstname;
     //     this.lastname = lastname;
     // }
+    const User = function (firstname,lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    let newuser = new User(inputFieldFirstName.value, inputFieldLastName.value)
 
-    // let newuser = new User(inputFieldFirstName.value, inputFieldLastName.value)
-    app.post("/users", users.create(inputFieldFirstName.value, )); 
+    console.log(newuser)
+    // app.post("/users", () )); 
 
+    app.post("/users", newuser);
     
 
-    app.post('/users', (req, res) => {
-        let user = {
-            firstname: inputFieldFirstName.value,
-            lastname: inputFieldLastName.value
-        }
-        return res.send('Received a POST HTTP method');
-      });
-    console.log(newuser)
+    // app.post('/users', (req, res) => {
+    //     let user = {
+    //         firstname: inputFieldFirstName.value,
+    //         lastname: inputFieldLastName.value
+    //     }
+    //     return res.send('Received a POST HTTP method');
+    //   });
+    // console.log(newuser)
 }
 
 
